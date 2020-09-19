487bea15369f990362e97c335d398fa4973a5b11
DMC09 <Dharminlive12@live.com>
2020-09-19 15:52:36 -0500

    implemented .env

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// modules
 const express = require('express')
-const app = express()
-const port = 3001
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const port = process.env.PORT ;
+
+// gen setup
+const app = express()
+dotenv.config();
+
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cors())
+
+
 
 
 app.get('/', (req, res) => {
@@ -15,14 +24,8 @@ app.get('/', (req, res) => {
 
 app.post('/post', (req, res) => {
   console.log(req.body)
-
-
-})
-
-app.get('/test', (req, res) => {
-console.log('you hit the test congrats!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
