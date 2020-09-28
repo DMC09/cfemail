f69bd8bf9e79f69eba0db2e6beaa541d98308de3
DMC09 <Dharminlive12@live.com>
2020-09-27 20:55:33 -0500

    using new input field and color pallete.

--- src/container/InitialForm.js
+++ src/container/InitialForm.js
@@ -23,141 +23,148 @@ export default function InitialForm() {
     await setComplete(true);
   };
   return (
-      <form className="form_container" onSubmit={handleSubmit(onSubmit)}>
+    <form className="form_container" onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
-      <h1 style={{textAlign:"center"}}>Sign up for the beta</h1>
+        <h1 style={{ textAlign: "center" }}>Sana beta signup form!</h1>
         <div className="name_row">
-        <div id="firstName" className="input-field">
-          <label label="first_name">First Name</label>
-          <input
-            id="first_name"
-            type="text"
-            className="validate"
-            name="fname"
-            ref={register({
-              required: true,
-              minLength: 1,
-              pattern: /^[a-zA-z]+$/,
-            })}
-          ></input>
-          {errors.fname?.type === "required" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input is required
-            </span>
-          )}
-          {errors.fname?.type === "minLength" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input needs to be at least 1 letter
-            </span>
-          )}
-          {errors.fname?.type === "pattern" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              please only use letters
-            </span>
-          )}
-        </div>
-        <div id="lastName" className="input-field ">
-          <label label="last_name">Last Name</label>
-          <input
-            id="last_name"
-            type="text"
-            className="validate"
-            name="lname"
-            ref={register({
-              required: true,
-              minLength: 1,
-              pattern: /^[a-zA-z]+$/,
-            })}
-          ></input>
-          {errors.lname?.type === "required" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input is required
-            </span>
-          )}
-          {errors.lname?.type === "minLength" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input needs to be at least 1 letter
-            </span>
-          )}
-          {errors.lname?.type === "pattern" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              please only use letters
-            </span>
-          )}
-        </div>
+          <div id="firstName" className=" group">
+            <input
+              id="first_name"
+              type="text"
+              className="validate"
+              name="fname"
+              required
+              ref={register({
+                required: true,
+                minLength: 1,
+                pattern: /^[a-zA-z]+$/,
+              })}
+            ></input>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label >First Name</label>
+            {errors.fname?.type === "required" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input is required
+              </span>
+            )}
+            {errors.fname?.type === "minLength" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input needs to be at least 1 letter
+              </span>
+            )}
+            {errors.fname?.type === "pattern" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                please only use letters
+              </span>
+            )}
+          </div>
+          {/* <div class="group">
+            <input type="text" required />
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Name</label>
+          </div> */}
+          {/* <div id="lastName" className="input-field ">
+            <label label="last_name">Last Name</label>
+            <input
+              id="last_name"
+              type="text"
+              className="validate"
+              name="lname"
+              ref={register({
+                required: true,
+                minLength: 1,
+                pattern: /^[a-zA-z]+$/,
+              })}
+            ></input>
+            {errors.lname?.type === "required" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input is required
+              </span>
+            )}
+            {errors.lname?.type === "minLength" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input needs to be at least 1 letter
+              </span>
+            )}
+            {errors.lname?.type === "pattern" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                please only use letters
+              </span>
+            )}
+          </div> */}
         </div>
         <div className="email_row">
-        <div id="email" className="input-field">
-          <label label="email">Email</label>
-          <input
-
-            type="email"
-            className="validate"
-            name="email"
-            ref={register({
-              required: true,
-              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            })}
-          ></input>
-          {errors.email?.type === "required" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input is required
-            </span>
-          )}
-          {errors.email?.type === "minLength" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Your input needs to be at least 1 letter
-            </span>
-          )}
-          {errors.email?.type === "pattern" && (
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Please enter a valid email address
-            </span>
-          )}
-        </div>
+          <div id="email" className="input-field">
+            <label label="email">Email</label>
+            <input
+              type="email"
+              className="validate"
+              name="email"
+              ref={register({
+                required: true,
+                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
+            ></input>
+            {errors.email?.type === "required" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input is required
+              </span>
+            )}
+            {errors.email?.type === "minLength" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Your input needs to be at least 1 letter
+              </span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span
+                className="helper-text"
+                data-error="wrong"
+                data-success="right"
+              >
+                Please enter a valid email address
+              </span>
+            )}
+          </div>
         </div>
 
         <button className="btn next-btn" type="submit" name="action">
-        Next
+          Next
         </button>
-        </fieldset>
-      </form>
-
+      </fieldset>
+    </form>
   );
 }
